1b13db6032537ae962dacd98bf3786fd4a041918
Liliana Lozano <lilianalozanosc@gmail.com>
2021-05-03 22:09:06 -0500

    tarea2

---
+++ src/Tarea2/Main.java
@@ -0,0 +1,16 @@
+
+package Tarea2;
+
+/**
+ *
+ * @author Liliana Lozano
+ */
+public class Main {
+
+   
+    public static void main(String[] args) {
+        Usuario usuario = new Usuario();
+        Cliente cliente = new Cliente();
+    }
+    
+}
